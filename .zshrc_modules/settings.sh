c11ba44e79571debebac1269820a0c562426ecbc
yevhen orlov <yevhenorlov@gmail.com>
2022-11-21 19:03:36 +0100

    replace prompt greeting with neofetch

--- .zshrc_modules/settings.sh
+++ .zshrc_modules/settings.sh
@@ -5,15 +5,4 @@
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
-
-# prompt greeting
-h=`date +%H`
-if [ $h -lt 12 ]; then
-  TIMEOFDAY="morning"
-elif [ $h -lt 18 ]; then
-  TIMEOFDAY="afternoon"
-else
-  TIMEOFDAY="evening"
-fi
-echo "Good ${TIMEOFDAY}, Yevhen."
-
+neofetch
--- .zshrc_modules/theme.sh
+++ .zshrc_modules/theme.sh
@@ -2,5 +2,4 @@
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-#ZSH_THEME="cobalt2" # not included by default, see https://github.com/wesbos/Cobalt2-iterm
 ZSH_THEME="minimal"
