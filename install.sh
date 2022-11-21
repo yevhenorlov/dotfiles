25dd010e198c4c0dbea970ca777887954c8996f6
yevhen orlov <yevhenorlov@gmail.com>
2022-11-21 19:27:10 +0100

    add ayu color scheme to kitty config

--- install.sh
+++ install.sh
@@ -12,23 +12,24 @@ else
 fi
 
 if [[ -d "$ConfigPath" ]]; then 
- : 
+ : # noop
 else
   echo "Config path $ConfigPath does not exist. Creating..."
-  # check if nvim directory exists
-  if [[ -d "$ConfigPath/nvim" ]]; then
-    :
-    else 
-      echo "Creating nvim directory..."
-      mkdir -p "$ConfigPath/nvim"
-  fi
-  # check if kitty directory exists
-  if [[ -d "$ConfigPath/kitty" ]]; then
-    :
-    else 
-      echo "Creating kitty directory..."
-      mkdir -p "$ConfigPath/kitty"
-  fi
+  mkdir "$ConfigPath"
+fi
+# check if nvim directory exists
+if [[ -d "$ConfigPath/nvim" ]]; then
+  : # noop
+else 
+  echo "Creating nvim directory..."
+  mkdir -p "$ConfigPath/nvim"
+fi
+# check if kitty directory exists
+if [[ -d "$ConfigPath/kitty" ]]; then
+  : # noop
+else 
+  echo "Creating kitty directory..."
+  mkdir -p "$ConfigPath/kitty"
 fi
 
 # zsh
@@ -48,3 +49,5 @@ ln -sfn "$DotfilesPath/nvim/init.vim" "$ConfigPath/nvim/init.vim"
 # kitty
 ln -sfn "$DotfilesPath/kitty/kitty.conf" "$ConfigPath/kitty/kitty.conf"
 ln -sfn "$DotfilesPath/kitty/gruvbox_dark.conf" "$ConfigPath/kitty/gruvbox_dark.conf"
+ln -sfn "$DotfilesPath/kitty/gruvbox_dark.conf" "$ConfigPath/kitty/gruvbox_light_soft.conf"
+ln -sfn "$DotfilesPath/kitty/ayu.conf" "$ConfigPath/kitty/ayu.conf"
