df7293453c3967b2cfb716a91b913d2e8503db14
yevhen orlov <yevhenorlov@gmail.com>
2023-10-30 20:58:04 +0100

    update telescope

--- nvim/lua/packer_plugins.lua
+++ nvim/lua/packer_plugins.lua
@@ -36,7 +36,7 @@ return require("packer").startup(function(use)
 
   -- neovim-native fuzzy finder
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   require("telescope").setup{
