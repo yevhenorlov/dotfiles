4b8467bc6f5660af70cb3fcab0dcf11a4ed3058b
yevhen orlov <yevhenorlov@gmail.com>
2024-03-15 09:38:42 +0100

    neovim: add emmet lsp

--- nvim/lua/yvhn/lsp.lua
+++ nvim/lua/yvhn/lsp.lua
@@ -56,6 +56,22 @@ on_attach = function()
 end,
 }
 
+local emmet_capabilities = vim.lsp.protocol.make_client_capabilities()
+emmet_capabilities.textDocument.completion.completionItem.snippetSupport = true
+require'lspconfig'.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = emmet_capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+
 -- FORMATTING
 
 local null_ls = require("null-ls")
