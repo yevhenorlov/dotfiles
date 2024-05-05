f4a1625139a41a4fe2a0395a3cac0343bec4073c
yevhen orlov <yevhenorlov@gmail.com>
2024-05-05 16:50:36 +0200

    wip statusline and other plugins

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -10,13 +10,15 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-  "nvim-lua/plenary.nvim", -- async core dependency
 
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+require("lazy").setup({
   "neovim/nvim-lspconfig",-- Configurations for Nvim LSP
   "jose-elias-alvarez/null-ls.nvim", -- formatting, linting
 
-	{'rose-pine/neovim', as = 'rose-pine'}, -- colorscheme
+	{'rose-pine/neovim', as = 'rose-pine', priority = 1000, }, -- colorscheme
 	"preservim/nerdtree", -- NERDTree - file explorer for vim
 
   -- code completions (setup in cmp.lua)
@@ -43,8 +45,14 @@ require("lazy").setup({
 	"tpope/vim-surround",	-- Surround words with brackets
 
   -- neovim-native fuzzy finder
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
-
+  { 'nvim-telescope/telescope.nvim', 
+    event = 'VimEnter', 
+    branch = '0.1.x', 
+    dependencies = { 
+      'nvim-lua/plenary.nvim',
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    }
+  },
   -- Harpoon (persistent project-level markers)
   { "ThePrimeagen/harpoon", branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } },
 
@@ -52,5 +60,85 @@ require("lazy").setup({
 	"editorconfig/editorconfig-vim", -- Generic .editorconfig standard support for Vim
 	"junegunn/vim-easy-align", -- A simple, easy-to-use Vim alignment plugin
 	"github/copilot.vim", -- Github copilot
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    dependencies = { 
+      'lewis6991/gitsigns.nvim',
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { 
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+            local git           = MiniStatusline.section_git({ trunc_width = 75 })
+            local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+            local location      = MiniStatusline.section_location({ trunc_width = 75 })
+            local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl,                  strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = mode_hl,                  strings = { search, location } },
+            })
+          end
+        },
+        use_icons = vim.g.have_nerd_font,
+      }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+  -- NOTE 
 })
 
