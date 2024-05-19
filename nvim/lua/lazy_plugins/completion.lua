f8471d254f9a4bd2b30bb27d06d9f887f31eab04
yevhen orlov <yevhenorlov@gmail.com>
2024-05-19 18:52:34 +0200

    refactor cmp (completion) module

---
+++ nvim/lua/lazy_plugins/completion.lua
@@ -0,0 +1,20 @@
+-- code completions
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    priority = 100,
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      -- "hrsh7th/cmp-nvim-lua", -- TODO not needed?
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      require "yvhn.completion"
+    end,
+  },
+}
