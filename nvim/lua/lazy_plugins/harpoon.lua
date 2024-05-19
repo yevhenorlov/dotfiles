c709599f1feaabd6c09857d47db80d09240d98b8
yevhen orlov <yevhenorlov@gmail.com>
2024-05-19 19:51:51 +0200

    refactor harpoon module

--- nvim/lua/lazy_plugins/harpoon.lua
+++ nvim/lua/lazy_plugins/harpoon.lua
@@ -2,5 +2,8 @@
 return { 
   "ThePrimeagen/harpoon", 
   branch = "harpoon2", 
-  dependencies = { "nvim-lua/plenary.nvim" } 
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("yvhn.harpoon")
+  end
 }
