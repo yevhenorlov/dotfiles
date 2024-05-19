c501136154956e54175a0444dffce11a50d5aba4
yevhen orlov <yevhenorlov@gmail.com>
2024-05-19 09:46:30 +0200

    duck!

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -154,6 +154,12 @@ require("lazy").setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  -- NOTE 
+  {
+    'tamton-aquib/duck.nvim',
+    config = function()
+        vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🪿") end, {})
+        vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
+        vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
+    end
+  },
 })
-
