34ce14220bfff0662b90bb8f959f72e0ffb69583
yevhen orlov <yevhenorlov@gmail.com>
2024-05-20 11:07:26 +0200

    make Meta key combos work in kitty
    also cleanup

--- nvim/lua/yvhn/commands.lua
+++ nvim/lua/yvhn/commands.lua
@@ -1,9 +1,22 @@
 local nnoremap = require("yvhn.keymap").nnoremap
-local vnoremap = require("yvhn.keymap").vnoremap
--- TODO move these functions into their respective modules and call them natively from lua
 
-local is_zen_mode = false
-function toggle_zen_mode()
+-- toggle relative number
+vim.api.nvim_create_user_command("ToggleRelativeNumber", function()
+	local is_relative_number = vim.o.relativenumber
+	if is_relative_number then
+		vim.o.relativenumber = false
+	else
+		vim.o.relativenumber = true
+	end
+end, {})
+nnoremap("<Leader>tn", vim.cmd.ToggleRelativeNumber, {
+	desc = "Toggle between absolute and relative line numbers",
+	silent = true,
+})
+
+-- toggle zen mode
+vim.api.nvim_create_user_command("ToggleZenMode", function()
+	local is_zen_mode = vim.o.showcmd and vim.o.number
 	if is_zen_mode then
 		vim.opt.showcmd = false
 		vim.opt.laststatus = 0
@@ -15,71 +28,25 @@ function toggle_zen_mode()
 		vim.opt.number = true
 		vim.opt.relativenumber = false
 	end
-	is_zen_mode = not is_zen_mode
-end
+end, {})
+nnoremap("<Leader>tz", vim.cmd.ToggleZenMode, {
+	desc = "Remove distractions and focus on the code",
+	silent = true,
+})
 
-local is_relative_number = false
-function toggle_relative_number()
-	if is_relative_number then
-		vim.opt.relativenumber = false
-	else
-		vim.opt.relativenumber = true
-	end
-	is_relative_number = not is_relative_number
-end
-
-local is_lsp_on = true
-function toggle_lsp()
-	if is_lsp_on then
-		vim.cmd(":LspStop")
-		print(":LspStop") -- TODO figure out why cmd doesn't print automatically
-	else
-		vim.cmd(":LspStart")
-		print(":LspStart")
-	end
-	is_lsp_on = not is_lsp_on
-end
-
+-- toggle copilot
 local is_copilot_enabled = true
-function toggle_copilot()
+vim.api.nvim_create_user_command("ToggleCopilot", function()
 	if is_copilot_enabled then
 		vim.cmd(":Copilot disable")
-		print(":Copilot disable") -- TODO figure out why cmd doesn't print automatically
+		print("Copilot disabled")
 	else
 		vim.cmd(":Copilot enable")
-		print(":Copilot enable")
+		print("Copilot enabled")
 	end
 	is_copilot_enabled = not is_copilot_enabled
-end
-
-local is_wrap_enabled = false
-function toggle_wrap()
-	vim.opt.wrap = true
-	vim.opt.list = false
-	if is_wrap_enabled then
-		vim.opt.linebreak = false
-		vim.keymap.del("n", "j")
-		vim.keymap.del("n", "k")
-		vim.keymap.del("n", "0")
-		vim.keymap.del("n", "^")
-		vim.keymap.del("n", "$")
-		vim.keymap.del("v", "j")
-		vim.keymap.del("v", "k")
-		vim.keymap.del("v", "0")
-		vim.keymap.del("v", "^")
-		vim.keymap.del("v", "$")
-	else
-		vim.opt.linebreak = true
-		nnoremap("j", "gj")
-		nnoremap("k", "gk")
-		nnoremap("0", "g0")
-		nnoremap("^", "g^")
-		nnoremap("$", "g$")
-		vnoremap("j", "gj")
-		vnoremap("k", "gk")
-		vnoremap("0", "g0")
-		vnoremap("^", "g^")
-		vnoremap("$", "g$")
-	end
-	is_wrap_enabled = not is_wrap_enabled
-end
+end, {})
+nnoremap("<Leader>tc", vim.cmd.ToggleCopilot, {
+	desc = "Toggle Copilot",
+})
+nnoremap("<Leader>cs", "<cmd>:Copilot status<CR>")
