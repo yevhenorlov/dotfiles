b25347b025868323d4272ae235705f6de360d3d9
yevhen orlov <yevhenorlov@gmail.com>
2024-12-08 14:37:18 +0100

    cleanup

--- zsh/.zshrc_modules/paths.sh
+++ zsh/.zshrc_modules/paths.sh
@@ -1,7 +1,6 @@
 # If you come from bash you might have to change your $PATH.
 export PATH=$HOME/bin:/usr/local/bin:$PATH
-# Path to chatgpt
-export PATH=$PATH:$HOME/chatgpt.sh
+export PATH=$PATH:$HOME/chatgpt.sh # Path to chatgpt
 export PATH=$HOME/.composer/vendor/bin:$PATH
 export PATH=$PATH:/usr/local/go/bin
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
