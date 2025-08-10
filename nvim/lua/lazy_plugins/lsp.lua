feff620d4b4dd2c3e8482d12c4d55642ed3d6273
yevhen orlov <yevhenorlov@gmail.com>
2025-08-10 22:42:52 +0200

    comment out ocaml config

--- nvim/lua/lazy_plugins/lsp.lua
+++ nvim/lua/lazy_plugins/lsp.lua
@@ -54,7 +54,7 @@ return {
 				prismals = true,
 				rust_analyzer = true,
 				tailwindcss = true,
-				ocamllsp = true,
+				-- ocamllsp = true,
 				vue_ls = true, -- runs in tandem with ts_ls (formerly tsserver) (https://github.com/vuejs/language-tools/blob/0e52a2d21fdd7c68447b7cd3d5c06876762cdc8b/README.md?plain=1#L33)
 
 				ts_ls = {
@@ -131,7 +131,7 @@ return {
 			local ensure_installed = {
 				"eslint_d",
 				"lua_ls",
-				"ocamlformat",
+				-- "ocamlformat",
 				"prettierd",
 				"shfmt",
 				"stylua",
@@ -197,7 +197,7 @@ return {
 				formatters_by_ft = {
 					bash = { "shfmt" },
 					lua = { "stylua" },
-					ocaml = { "ocamlformat" },
+					-- ocaml = { "ocamlformat" },
 					typescript = { "eslint_d", "prettierd" },
 					typescriptreact = { "eslint_d", "prettierd" },
 					javascript = { "eslint_d", "prettierd" },
